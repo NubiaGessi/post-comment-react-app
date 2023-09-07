3b4d6534a6cfa04215bd2e00a8831c157b56bb6b
bhirosse <brunabino.h@gmail.com>
2023-09-07 17:15:56 -0400

    branch test

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -2,11 +2,11 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('localhost:5173/')
     cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="comment-input"]').first().type('test 1234')
     cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').contains('test 1234')
     cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
+      if ($element.text() === 'test 1234') {
         cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
       }
     })
